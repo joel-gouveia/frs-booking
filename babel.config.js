871a929dedf65fa78c68d1fe412fcebe8ff6af33
Matheus Vaillant <matheusvaillant505@gmail.com>
2023-11-29 12:43:42 +0000

    chore: Path aliases with babel

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,36 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          extensions: [
+            '.ios.js',
+            '.android.js',
+            '.ios.jsx',
+            '.android.jsx',
+            '.js',
+            '.jsx',
+            '.json',
+            '.ts',
+            '.tsx',
+          ],
+          root: ['.'],
+          alias: {
+            '@screens': './src/screens',
+            '@components': './src/components',
+            '@constants': './src/constants',
+            '@utils': './src/utils',
+            '@layout': './src/layout',
+            '@navigation': './src/navigation',
+            '@hooks': './src/hooks',
+            '@api': './src/api',
+            '@types': './src/types',
+          },
+        },
+      ],
+    ],
+  };
 };
