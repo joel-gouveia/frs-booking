d95fa82b4eb67034befcf66f4ef0c752e688e1e6
João Queiroz <joao.queiroz@hydradev.io>
2023-12-04 20:10:22 +0000

    Reverse pre-commit

--- src/types/i18n/resources.d.ts
+++ src/types/i18n/resources.d.ts
@@ -1,16 +1,16 @@
 interface Resources {
-  "de": {
-    "login": {
-      "username": "Nutzername",
-      "password": "Passwort"
-    }
-  },
-  "en": {
-    "login": {
-      "username": "Username",
-      "password": "Password"
-    }
-  }
+  de: {
+    login: {
+      username: "Nutzername";
+      password: "Passwort";
+    };
+  };
+  en: {
+    login: {
+      username: "Username";
+      password: "Password";
+    };
+  };
 }
 
 export default Resources;
