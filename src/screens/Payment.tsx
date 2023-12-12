389eee60e46773db564291801ed1888418101113
João Queiroz <joao.queiroz@hydradev.io>
2023-12-12 16:56:47 +0000

    Add cash card buttons

--- src/screens/Payment.tsx
+++ src/screens/Payment.tsx
@@ -1,6 +1,6 @@
 import { useTranslation } from "react-i18next";
 import { useNavigation } from "@react-navigation/native";
-import { Typography, VStack } from "@components/index";
+import { Button, HStack, Typography, VStack } from "@components/index";
 import { useBooking } from "@hooks/useBooking";
 import { getTodayDateString } from "@utils/date";
 import React, { useMemo } from "react";
@@ -8,6 +8,7 @@ import { StyleSheet, View } from "react-native";
 import { ScreenLayout } from "src/layouts/ScreenLayout";
 import { Footer } from "@components/Footer/Footer";
 import { NavigationProps, NavigationScreens } from "src/types/navigation";
+import Icon from "react-native-vector-icons/Feather";
 
 export function PaymentScreen() {
   const { t } = useTranslation();
@@ -23,8 +24,28 @@ export function PaymentScreen() {
           Voyageleg: {today} {originCode} - {destinationCode}
         </Typography>
       </View>
-      <VStack gap={20}>
+      <VStack gap={24}>
         <Typography size="small">Payment Method</Typography>
+        <HStack gap={20}>
+          <Button variant="outline" style={{ flex: 1, paddingVertical: 40, marginLeft: 10 }}>
+            <Typography fontSize={11} style={{ position: "absolute", right: 4, top: 4 }}>
+              1
+            </Typography>
+            <Icon name="dollar-sign" size={60} color="black" />
+            <Typography fontSize={26} style={{ position: "absolute", bottom: 6 }}>
+              Cash
+            </Typography>
+          </Button>
+          <Button variant="outline" style={{ flex: 1, paddingVertical: 40, marginRight: 10 }}>
+            <Typography fontSize={11} style={{ position: "absolute", right: 4, top: 4 }}>
+              2
+            </Typography>
+            <Icon name="credit-card" size={60} color="black" />
+            <Typography fontSize={26} style={{ position: "absolute", bottom: 6 }}>
+              Card
+            </Typography>
+          </Button>
+        </HStack>
       </VStack>
       <Footer
         buttons={[
@@ -48,7 +69,7 @@ export function PaymentScreen() {
 
 const styles = StyleSheet.create({
   header: {
-    marginBottom: 80,
+    marginBottom: 24,
   },
   headerText: {
     backgroundColor: "#d9d9d9",
